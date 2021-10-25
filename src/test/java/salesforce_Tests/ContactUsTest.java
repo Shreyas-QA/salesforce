bdd58b3113132824be357a1caad1c9619c9a04af
shreyas <sambareshreyas@gmail.com>
2021-10-25 14:18:30 +0530

    Test Commit

--- src/test/java/salesforce_Tests/ContactUsTest.java
+++ src/test/java/salesforce_Tests/ContactUsTest.java
@@ -10,17 +10,18 @@ import salesforce_Pages.HomePage;
 public class ContactUsTest extends BaseTest {
 	private ContactUs_Pages cpage;
 
-	@Test
+	@Test(priority = 1)
 	public void ClickOnContactUS_Link() {
 		cpage = PageFactory.initElements(driver, ContactUs_Pages.class);
 		cpage.ClickOnContactUs();
 	}
 
-	@Test
+	@Test(priority = 2)
 	public void GoToContactUsPage() {
+		
 		ArrayList<String> tabs2 = new ArrayList<String>(driver.getWindowHandles());
 		driver.switchTo().window(tabs2.get(1));
 		driver.getTitle();
-		System.out.println();
+
 	}
 }
